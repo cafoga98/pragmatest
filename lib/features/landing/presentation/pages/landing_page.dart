090d8fd2156faf6bf1c734f4f5b07790ce34bc8b
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-31 17:51:24 -0500

    Add new landing_page.dart to show list the cats

--- lib/features/landing/presentation/pages/landing_page.dart
+++ lib/features/landing/presentation/pages/landing_page.dart
@@ -1,5 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pragmatest/core/shared/utils/colors_repository.dart';
+import 'package:pragmatest/core/shared/utils/images_reporitory.dart';
+import 'package:pragmatest/core/shared/utils/style_repository.dart';
+import 'package:pragmatest/features/landing/domain/blocs/cat_bloc/cat_bloc.dart';
+import 'package:pragmatest/features/landing/presentation/widgets/card_cat.dart';
+import 'package:pragmatest/generated/l10n.dart';
 
 @RoutePage()
 class LandingPage extends StatelessWidget {
@@ -7,6 +14,61 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorsRepository.teal,
+      appBar: AppBar(
+        title: Text(
+          S.current.nameApp,
+        ),
+      ),
+      body: Column(
+        children: [
+          BlocBuilder<CatBloc, CatState>(
+            builder: (context, state) => Expanded(
+              flex: 8,
+              child: state.maybeWhen(
+                orElse: () {
+                  return Container();
+                },
+                catLoading: () {
+                  return Center(
+                    child: Image.asset(
+                      ImageRepository.loadingNoBackground,
+                    ),
+                  );
+                },
+                catLoaded: (catsList) {
+                  if (catsList.isNotEmpty) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.all(10),
+                      itemCount: catsList.length,
+                      itemBuilder: (context, index) {
+                        return CardCat(catBreed: catsList[index]);
+                      },
+                    );
+                  } else {
+                    return Center(
+                      child: Text(
+                        S.current.noKittens,
+                        style: medium,
+                      ),
+                    );
+                  }
+                },
+                catError: (e) {
+                  return Center(
+                    child: Text(
+                      e,
+                      style: medium,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
