d5d92c0034af830933a53d4af4efa45fbaeab96b
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-31 11:35:11 -0500

    Add colors palette

---
+++ lib/core/shared/utils/colors_repository.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/animation.dart';
+
+class ColorsRepository {
+  ColorsRepository._();
+
+  ///Principal color
+  static const black = Color(0xFF000000);
+  static const brown = Color(0xFF734226);
+  static const teal = Color(0xFF1c505d);
+  static const darkGreen = Color(0xFF212517);
+}
