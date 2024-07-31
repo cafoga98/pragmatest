4b5e0abcd1063f071a58e8c5ea505cad26c26287
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-31 17:44:36 -0500

    Add loading gif

--- lib/core/shared/utils/images_reporitory.dart
+++ lib/core/shared/utils/images_reporitory.dart
@@ -2,4 +2,6 @@ class ImageRepository {
   ImageRepository._();
   static const catLogo = 'assets/img/logo_app.png';
   static const catOfficialImg = 'assets/img/cat_image.png';
+  static const loading = 'assets/img/loading.gif';
+  static const loadingNoBackground = 'assets/img/loading_no_brackground.gif';
 }
