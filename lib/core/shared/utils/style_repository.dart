36cfab459b0ed63c83ff686d716795340c2c3afc
cafoga98 <camiloforerogalindo@gmail.com>
2024-07-31 11:35:39 -0500

    Add style repository

---
+++ lib/core/shared/utils/style_repository.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '/core/shared/utils/colors_repository.dart';
+
+final extraLarge = TextStyle(
+  fontFamily: 'SignikaNegative',
+  fontWeight: FontWeight.bold,
+  fontSize: 31.sp,
+);
+
+final large = TextStyle(
+  fontFamily: 'SignikaNegative',
+  fontSize: 26.sp,
+);
+
+final medium = TextStyle(
+  fontFamily: 'ChakraPetch',
+  fontSize: 19.sp,
+  fontWeight: FontWeight.bold,
+);
+
+final small = TextStyle(
+  fontFamily: 'ChakraPetch',
+  fontSize: 17.sp,
+);
+
+InputDecoration inputTextFormField = InputDecoration(
+  hintStyle: medium.copyWith(color: Colors.grey),
+  isDense: true,
+  filled: true,
+  fillColor: Colors.white,
+  errorBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(20.0),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(20.0),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(20.0),
+  ),
+);
